5432a4740b9a74bdb22e76ba20a92f627f6ee2c7
Jose Seie <joseeugenioseie@hotmail.com>
2017-11-28 10:52:47 +0200

    Metodo para impressao no LogCat, funcionando com sucesso

--- eXcript/Aula02/app/src/main/java/io/github/joseseie/aula02/Main.java
+++ eXcript/Aula02/app/src/main/java/io/github/joseseie/aula02/Main.java
@@ -2,12 +2,20 @@ package io.github.joseseie.aula02;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class Main extends Activity {
 
     protected void onCreate(Bundle save) {
         super.onCreate(save);
         setContentView(R.layout.main);
+    }
+
+    public static void cliquei(View v) {
+
+        Log.i("Ola","Eu passei por aqui.");
+
     }
 
 
