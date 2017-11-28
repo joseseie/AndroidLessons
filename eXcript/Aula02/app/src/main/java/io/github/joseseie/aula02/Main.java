c7aea948a98e1c1a0b009e610e46d04a8861b3e8
Jose Seie <joseeugenioseie@hotmail.com>
2017-11-28 10:41:36 +0200

    Fim da aula 02, adicao do botao e metodo cliquei

--- eXcript/Aula02/app/src/main/java/io/github/joseseie/aula02/Main.java
+++ eXcript/Aula02/app/src/main/java/io/github/joseseie/aula02/Main.java
@@ -1,2 +1,14 @@
 package io.github.joseseie.aula02;
 
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main extends Activity {
+
+    protected void onCreate(Bundle save) {
+        super.onCreate(save);
+    }
+
+
+
+}
