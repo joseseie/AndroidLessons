f5a06c6379a3e707e6f315ac6bbda989f60626dc
Jose Seie <joseeugenioseie@hotmail.com>
2017-12-19 11:05:03 +0200

    Implementado evento Clique e já foi feita a validacao dos campos

--- CalculadoraBasica/app/src/main/java/io/github/joseseie/calculadorabasica/ActMain.java
+++ CalculadoraBasica/app/src/main/java/io/github/joseseie/calculadorabasica/ActMain.java
@@ -1,11 +1,16 @@
 package io.github.joseseie.calculadorabasica;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.*;
 import android.widget.*;
 
-public class ActMain extends AppCompatActivity {
+public class ActMain extends AppCompatActivity implements View.OnClickListener{
+
+    /*
+    * Nota: Para acao de click usanos View.OnClickListener
+    */
 
 //    Criacao de objectos (By Seie)
     private EditText edtValor1;
@@ -23,6 +28,42 @@ public class ActMain extends AppCompatActivity {
         edtValor2 = (EditText) findViewById(R.id.edtValor2);
         btnCalcular =  (Button) findViewById(R.id.btnCalcular);
 
+//        Outra forma de vincular o evento onClick.
+//        btnCalcular.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v){}
+//        });
+
+        btnCalcular.setOnClickListener(this);
 
     }
+
+    @Override
+    public void onClick(View v)
+    {
+        AlertDialog.Builder dlg = new AlertDialog.Builder(this);
+        String v1 = this.edtValor1.getText().toString();
+        String v2 = this.edtValor2.getText().toString();
+
+        if(v1.trim().isEmpty() || v2.trim().isEmpty())
+        {
+            dlg.setMessage("Por favor preencha todos campos");
+            dlg.setNeutralButton("Ok", null);
+            dlg.show();
+        }
+        else {
+
+            double valor1 = Double.parseDouble(v1);
+            double valor2 = Double.parseDouble(v2);
+
+            double resultado = valor1 + valor2;
+
+            dlg.setMessage("O resultado eh: " + resultado);
+            dlg.setNeutralButton("Ok", null);
+            dlg.show();
+
+        }
+
+    }
+
 }
