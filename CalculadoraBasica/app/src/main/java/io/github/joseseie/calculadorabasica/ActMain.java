e64894187647312ddb12885d31f43fbbf8dc08eb
Jose Seie <joseeugenioseie@hotmail.com>
2017-12-19 10:36:11 +0200

    Recuperando os valores nas views

--- CalculadoraBasica/app/src/main/java/io/github/joseseie/calculadorabasica/ActMain.java
+++ CalculadoraBasica/app/src/main/java/io/github/joseseie/calculadorabasica/ActMain.java
@@ -2,12 +2,27 @@ package io.github.joseseie.calculadorabasica;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.*;
+import android.widget.*;
 
 public class ActMain extends AppCompatActivity {
+
+//    Criacao de objectos (By Seie)
+    private EditText edtValor1;
+    private EditText edtValor2;
+    private Button btnCalcular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_main);
+
+//        Recuperacao dos valores nas telas
+
+        edtValor1 = (EditText) findViewById(R.id.edtValor1);
+        edtValor2 = (EditText) findViewById(R.id.edtValor2);
+        btnCalcular =  (Button) findViewById(R.id.btnCalcular);
+
+
     }
 }
