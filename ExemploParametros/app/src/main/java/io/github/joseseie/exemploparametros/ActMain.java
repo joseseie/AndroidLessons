6f00fc49ee0ea12f818c8cac6fe8f2f6c93dd173
Jose Seie <joseeugenioseie@hotmail.com>
2017-12-27 23:50:46 +0200

    Implementacao da Interface de evento

--- ExemploParametros/app/src/main/java/io/github/joseseie/exemploparametros/ActMain.java
+++ ExemploParametros/app/src/main/java/io/github/joseseie/exemploparametros/ActMain.java
@@ -5,11 +5,28 @@ import android.os.Bundle;
 import android.widget.*;
 import android.view.*;
 
-public class ActMain extends AppCompatActivity {
+public class ActMain extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText edtValor;
+    private Button btnOk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_main);
+
+        edtValor = (EditText) findViewById(R.id.edtValor);
+        btnOk = (Button) findViewById(R.id.btnOk);
+
     }
+
+    public void onClick(View v)
+    {
+        
+
+
+
+    }
+
+
 }
