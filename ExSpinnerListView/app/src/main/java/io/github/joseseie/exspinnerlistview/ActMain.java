d5cede2de8b352de8a2626ba3c57e7bc38708d24
Jose Seie <joseeugenioseie@hotmail.com>
2017-12-29 23:18:50 +0200

    Declaracao dos objectos

--- ExSpinnerListView/app/src/main/java/io/github/joseseie/exspinnerlistview/ActMain.java
+++ ExSpinnerListView/app/src/main/java/io/github/joseseie/exspinnerlistview/ActMain.java
@@ -2,8 +2,18 @@ package io.github.joseseie.exspinnerlistview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.*;
+import android.widget.*;
+
 
 public class ActMain extends AppCompatActivity {
+
+    private EditText edtValor;
+    private Spinner spnOpcoes;
+    private Button btnAdicionar;
+    private Button btnExcluir;
+    private ListView lstDados;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
