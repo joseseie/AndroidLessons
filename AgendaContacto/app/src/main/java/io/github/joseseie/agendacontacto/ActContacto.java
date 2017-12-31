45695b676b18ae839cd9175b276184a80a89e268
Jose Seie <joseeugenioseie@hotmail.com>
2017-12-31 02:17:46 +0200

    Usando o Intent para chamar a outra tela

--- AgendaContacto/app/src/main/java/io/github/joseseie/agendacontacto/ActContacto.java
+++ AgendaContacto/app/src/main/java/io/github/joseseie/agendacontacto/ActContacto.java
@@ -1,13 +1,33 @@
 package io.github.joseseie.agendacontacto;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.*;
+import android.view.*;
 
-public class ActContacto extends AppCompatActivity {
+
+public class ActContacto extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageButton btnAdicionar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_contacto);
+
+        btnAdicionar = (ImageButton) findViewById(R.id.btnAdicionar);
+        btnAdicionar.setOnClickListener(this);
+
+
     }
+
+    public void onClick(View v)
+    {
+        Intent it = new Intent(this, ActCadContactos.class);
+
+        startActivity(it);
+
+    }
+
 }
