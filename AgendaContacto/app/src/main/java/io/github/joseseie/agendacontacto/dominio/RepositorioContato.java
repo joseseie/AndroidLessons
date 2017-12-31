8971e60a7c868e9aa273621184389bed11bb2184
Jose Seie <joseeugenioseie@hotmail.com>
2017-12-31 14:33:52 +0200

    Iniciada a iimplementacao do método buscaContatos()

--- AgendaContacto/app/src/main/java/io/github/joseseie/agendacontacto/dominio/RepositorioContato.java
+++ AgendaContacto/app/src/main/java/io/github/joseseie/agendacontacto/dominio/RepositorioContato.java
@@ -1,11 +1,30 @@
 package io.github.joseseie.agendacontacto.dominio;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.widget.ArrayAdapter;
+
 /**
  * Created by Jose Seie on 12/31/2017.
  */
 
 public class RepositorioContato {
 
-    
+    private SQLiteDatabase conn;
+
+
+    public RepositorioContato(SQLiteDatabase conn)
+    {
+        this.conn = conn;
+    }
+
+    public ArrayAdapter<String> buscaContatos(Context context)
+    {
+        ArrayAdapter<String> adpContactos = new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1);
+
+        //Continuar no minuto 20, no video 13.
+
+        return adpContactos;
+    }
 
 }
