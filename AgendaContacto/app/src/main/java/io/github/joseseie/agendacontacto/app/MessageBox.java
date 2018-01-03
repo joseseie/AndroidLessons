6b6825d2dda456030301746bdc21c03f35880926
Jose Seie <joseeugenioseie@hotmail.com>
2018-01-03 10:47:55 +0200

    Criacao de classe e metodos para mensagens

---
+++ AgendaContacto/app/src/main/java/io/github/joseseie/agendacontacto/app/MessageBox.java
@@ -0,0 +1,16 @@
+package io.github.joseseie.agendacontacto.app;
+
+import android.content.Context;
+
+/**
+ * Created by Jose Seie on 1/3/2018.
+ */
+
+public class MessageBox {
+
+    public static void show(Context ctx, String title, String msg)
+    {
+
+    }
+
+}
