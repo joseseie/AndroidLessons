38e71afc5013cb80258f92af6a5d9796f02fe324
Jose Seie <joseeugenioseie@hotmail.com>
2018-01-07 02:48:00 +0200

    Adicao da permissao de acesso á internet no androidManifest

--- ProjectoItent/app/src/main/java/io/github/joseseie/projectoitent/ChamarBrowser.java
+++ ProjectoItent/app/src/main/java/io/github/joseseie/projectoitent/ChamarBrowser.java
@@ -1,5 +1,8 @@
 package io.github.joseseie.projectoitent;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,13 +19,29 @@ public class ChamarBrowser extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chamar_browser);
 
-        this.btnAbrirBrowser = (Button) findViewById(R.id.btnAbrirBrowser);
+        this.btnAbrirBrowser    = (Button) findViewById(R.id.btnAbrirBrowser);
+        this.edtUrl             = (EditText) findViewById(R.id.edtUrl);
 
+        btnAbrirBrowser.setOnClickListener(this);
     }
 
 
     @Override
     public void onClick(View view) {
 
+        try {
+            Uri uri = Uri.parse(edtUrl.getText().toString());
+
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+
+            startActivity(intent);
+
+        } catch (Exception ex)
+        {
+            AlertDialog.Builder dlg = new AlertDialog.Builder(this);
+            dlg.setMessage("Url Invalida");
+            dlg.setNeutralButton("Ok",null);
+            dlg.show();
+        }
     }
 }
