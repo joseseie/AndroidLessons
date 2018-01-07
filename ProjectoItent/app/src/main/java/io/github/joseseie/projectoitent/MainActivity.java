9434dc3c95ccd59c31802a540631d1af59fc1f0a
Jose Seie <joseeugenioseie@hotmail.com>
2018-01-07 02:07:09 +0200

    Implementado o metodo onClick, chamando o outro activity com sucesso

--- ProjectoItent/app/src/main/java/io/github/joseseie/projectoitent/MainActivity.java
+++ ProjectoItent/app/src/main/java/io/github/joseseie/projectoitent/MainActivity.java
@@ -1,15 +1,34 @@
 package io.github.joseseie.projectoitent;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnChamarBroswer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.btnChamarBroswer = (Button) findViewById(R.id.btnChamarBroswer);
+
+        this.btnChamarBroswer.setOnClickListener(this);
     }
 
 
+    @Override
+    public void onClick(View view) {
+
+        if(view == btnChamarBroswer)
+        {
+            Intent intent = new Intent(this, ChamarBrowser.class );
+            startActivity(intent);
+        }
+    }
 }
