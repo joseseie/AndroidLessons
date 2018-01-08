8646c416f55739cdf3b0c547a2cee959f1d1b832
Jose Seie <joseeugenioseie@hotmail.com>
2018-01-08 05:32:30 +0200

    Ficheiro XML para Chamadas criada

---
+++ ProjectoItent/app/src/main/java/io/github/joseseie/projectoitent/FazerLigacao.java
@@ -0,0 +1,13 @@
+package io.github.joseseie.projectoitent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FazerLigacao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fazer_ligacao);
+    }
+}
