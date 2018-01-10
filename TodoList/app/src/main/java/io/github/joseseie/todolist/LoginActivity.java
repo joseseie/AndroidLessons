2503e0ad8f586bc2a8ef85807e7283665eb7a85f
Jose Seie <joseeugenioseie@hotmail.com>
2018-01-10 17:02:25 +0200

    Allgoritmo de manter conectado concluido

--- TodoList/app/src/main/java/io/github/joseseie/todolist/LoginActivity.java
+++ TodoList/app/src/main/java/io/github/joseseie/todolist/LoginActivity.java
@@ -1,9 +1,11 @@
 package io.github.joseseie.todolist;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 import dao.UsuarioDAO;
@@ -13,17 +15,30 @@ public class LoginActivity extends AppCompatActivity {
 
     private EditText edtUsuario, edtSenha;
     private UsuarioDAO helper;
+    private CheckBox ckbConectado;
+
+    private static final String MANTER_CONECTADO = "manter_conectado";
+    private static final String PREFERENCE_NAME = "LoginActivityPreferences";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        edtUsuario = (EditText) findViewById(R.id.login_edtUsuario);
-        edtSenha   = (EditText) findViewById(R.id.login_edtUSenha);
+        edtUsuario      = (EditText) findViewById(R.id.login_edtUsuario);
+        edtSenha        = (EditText) findViewById(R.id.login_edtUSenha);
+        ckbConectado    = (CheckBox) findViewById(R.id.login_ckbConectado);
+
 
         helper = new UsuarioDAO(this);
 
+        SharedPreferences preferences = getSharedPreferences(PREFERENCE_NAME, MODE_PRIVATE);
+        boolean conectado = preferences.getBoolean(MANTER_CONECTADO, false);
+
+        if (conectado)
+        {
+            chamarMainActivity();
+        }
     }
 
     public void logar(View view)
@@ -49,8 +64,17 @@ public class LoginActivity extends AppCompatActivity {
             // Logar
             if ( helper.logar(usuario, senha) )
             {
-                startActivity(new Intent(this, MainActivity.class));
-                finish();
+                if (ckbConectado.isChecked()) {
+                    SharedPreferences sharedPreferences = getSharedPreferences(PREFERENCE_NAME, MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                    editor.putBoolean(MANTER_CONECTADO, true);
+                    editor.commit();
+
+                }
+
+                chamarMainActivity();
+
             }
             else
             {
@@ -59,6 +83,12 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         }
+    }
+
+    private void chamarMainActivity()
+    {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
     }
 
 }
