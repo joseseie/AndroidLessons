29bf31cff3499744eaf57906558d80e3c2a45c9e
Jose Seie <joseeugenioseie@hotmail.com>
2018-01-10 18:42:18 +0200

    Chamando os menus, e implementacao de cadastro

---
+++ TodoList/app/src/main/java/io/github/joseseie/todolist/CadUsuarioActivity.java
@@ -0,0 +1,123 @@
+package io.github.joseseie.todolist;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+import dao.UsuarioDAO;
+import model.Usuario;
+import util.Mensagem;
+
+public class CadUsuarioActivity extends AppCompatActivity {
+
+    private EditText edtNome, edtLogin, edtSenha;
+    private UsuarioDAO usuarioDAO;
+    private Usuario usuario;
+    private int idusuario;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cad_usuario);
+
+        usuarioDAO = new UsuarioDAO(this);
+
+        edtNome         = (EditText) findViewById(R.id.usuario_edtNome);
+        edtLogin        = (EditText) findViewById(R.id.usuario_edtLogin);
+        edtSenha        = (EditText) findViewById(R.id.usuario_edtSenha);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        usuarioDAO.fechar();
+        super.onDestroy();
+    }
+
+    private void cadastrar()
+    {
+        boolean validacao = true;
+
+        String nome = edtNome.getText().toString();
+        String login = edtNome.getText().toString();
+        String senha = edtSenha.getText().toString();
+
+        if (nome == null || nome.equals("") ) {
+            validacao = false;
+            edtNome.setError(getString(R.string.campo_obrigatorio));
+        }
+
+        if (login == null || login.equals("") ) {
+            validacao = false;
+            edtNome.setError(getString(R.string.campo_obrigatorio));
+        }
+
+        if (senha == null || senha.equals("") ) {
+            validacao = false;
+            edtNome.setError(getString(R.string.campo_obrigatorio));
+        }
+
+        if ( validacao )
+        {
+            usuario = new Usuario();
+            usuario.setNome( nome );
+            usuario.setLogin( login );
+            usuario.setLogin( senha );
+
+            if ( idusuario > 0 )
+            {
+                usuario.set_id( idusuario );
+            }
+
+            long resultado = usuarioDAO.salvarUsuario(usuario);
+
+            if ( resultado != -1 )
+            {
+                if ( idusuario > 0 ) {
+                    Mensagem.Msg(this, getString(R.string.mensagem_actializar));
+                } else {
+                    Mensagem.Msg(this, getString(R.string.mensagem_cadastro));
+                }
+
+                finish();
+                startActivity( new Intent(this, MainActivity.class));
+            }
+            else
+            {
+                Mensagem.Msg(this, getString(R.string.mensagem_erro));
+            }
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.cadastros, menu);
+
+        if ( idusuario > 0 ) {
+            menu.findItem(R.id.action_menu_excluir).setVisible(true);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch ( id ) {
+            case R.id.action_menu_salvar:
+                this.cadastrar();
+                break;
+
+            case R.id.action_menu_sair:
+                finish();
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- TodoList/app/src/main/java/io/github/joseseie/todolist/MainActivity.java
+++ TodoList/app/src/main/java/io/github/joseseie/todolist/MainActivity.java
@@ -1,7 +1,10 @@
 package io.github.joseseie.todolist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if ( id == R.id.action_cadastro_usuarios ) {
+            startActivity( new Intent(this, CadUsuarioActivity.class) );
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
