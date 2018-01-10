f1753b588226440bc6f88598e86f8388c258dea9
Jose Seie <joseeugenioseie@hotmail.com>
2018-01-10 16:09:32 +0200

    Implementando a validacao de dados de login

--- TodoList/app/src/main/java/io/github/joseseie/todolist/LoginActivity.java
+++ TodoList/app/src/main/java/io/github/joseseie/todolist/LoginActivity.java
@@ -2,12 +2,44 @@ package io.github.joseseie.todolist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText edtUsuario, edtSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        edtUsuario = (EditText) findViewById(R.id.login_edtUsuario);
+        edtSenha   = (EditText) findViewById(R.id.login_edtUSenha);
     }
+
+    public void logar(View view)
+    {
+        String usuario = edtUsuario.getText().toString();
+        String senha = edtSenha.getText().toString();
+
+        boolean validacao = true;
+
+        if (usuario == null || usuario.equals(""))
+        {
+            validacao = false;
+            edtUsuario.setError(getString(R.string.login_valUsuario));
+        }
+
+        if (senha == null || senha.equals(""))
+        {
+            validacao = false;
+            edtUsuario.setError(getString(R.string.login_valSenha));
+        }
+
+        if( validacao ) {
+            // Logar..
+        }
+    }
+
 }
