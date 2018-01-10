424f9e7e418fa6a9dfadf2e8351909172a30afba
Jose Seie <joseeugenioseie@hotmail.com>
2018-01-10 15:22:03 +0200

    Conclusao da classe Database helper e criacao das classes Models

--- TodoList/app/src/main/java/dao/DatabaseHelper.java
+++ TodoList/app/src/main/java/dao/DatabaseHelper.java
@@ -19,7 +19,21 @@ public class DatabaseHelper extends SQLiteOpenHelper{
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    public void onCreate(SQLiteDatabase db) {
+        //Tabela de usuarios
+
+        db.execSQL("create table usuarios(" +
+                        "_id integer primary key autoincrement," +
+                        "nome text not null, " +
+                        "login text not null, " +
+                        "senha text not null) ");
+        //Tabela tarefas
+
+        db.execSQL("create table tarefas(_id integer primary key autoincrement, " +
+                "tarefa text not null, " +
+                "dt_criacao datetime default current_datetime, " +
+                "db_completado date )");
+
 
     }
 
@@ -27,4 +41,35 @@ public class DatabaseHelper extends SQLiteOpenHelper{
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
     }
+
+    public static class Usuarios {
+
+        public static final String TABELA = "usuarios";
+        public static final String _ID = "_id";
+        public static final String NOME = "nome";
+        public static final String LOGIN = "login";
+        public static final String SENHA = "senha";
+
+        public static final String[] COLUNAS = new String[]{
+                _ID, NOME, LOGIN, SENHA
+        };
+
+    }
+
+    public static class Tarefas {
+
+        public static final String TABELA = "tarefas";
+        public static final String _ID = "_id";
+        public static final String TAREFA = "tarefa";
+        public static final String DT_CRIACAO = "dt_criacao";
+        public static final String DT_COMPLETADO = "dt_completado";
+
+        public static final String[] COLUNAS = new String[]{
+                _ID, TAREFA, DT_CRIACAO, DT_COMPLETADO
+        };
+
+    }
+
+
+
 }
