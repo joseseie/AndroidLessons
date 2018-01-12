b179a8769ae2eb16e1656c4298dd6f9342af68b1
Jose Seie <joseeugenioseie@hotmail.com>
2018-01-12 17:57:50 +0200

    Reciclando as views

--- AndroidEatIt/app/src/main/java/io/github/joseseie/androideatit/Home.java
+++ AndroidEatIt/app/src/main/java/io/github/joseseie/androideatit/Home.java
@@ -3,6 +3,7 @@ package io.github.joseseie.androideatit;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -14,11 +15,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
+import io.github.joseseie.androideatit.Interface.ItemClickListener;
 import io.github.joseseie.androideatit.common.Common;
+import io.github.joseseie.androideatit.model.Category;
+import io.github.joseseie.androideatit.viewHolder.MenuViewHolder;
 
 public class Home extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -70,6 +77,34 @@ public class Home extends AppCompatActivity
         txtFullName.setText(Common.currentUser.getName());
 
         //Load menu
+        recycler_menu = (RecyclerView) findViewById(R.id.recycler_menu);
+        recycler_menu.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recycler_menu.setLayoutManager(layoutManager);
+
+        loadMenu();
+
+    }
+
+    private void loadMenu() {
+        FirebaseRecyclerAdapter<Category, MenuViewHolder> adapter = new FirebaseRecyclerAdapter<Category, MenuViewHolder>(Category.class, R.layout.menu_item, MenuViewHolder.class, category) {
+            @Override
+            protected void populateViewHolder(MenuViewHolder viewHolder, Category model, int position) {
+                viewHolder.txtMenuName.setText( model.getName() );
+                Picasso.with(getBaseContext()).load(model.getImage())
+                        .into(viewHolder.imageView);
+                final Category cllickItem = model;
+                viewHolder.setItemClickListener(new ItemClickListener() {
+                    @Override
+                    public void onClick(View view, int position, boolean isLongClick) {
+                        Toast.makeText(Home.this, ""+cllickItem.getName(), Toast.LENGTH_LONG).show();
+                    }
+                });
+
+            }
+        };
+
+        recycler_menu.setAdapter(adapter);
 
     }
 
@@ -92,15 +127,6 @@ public class Home extends AppCompatActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
@@ -111,17 +137,13 @@ public class Home extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_menu) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_cart) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_orders) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_log_out) {
 
         }
 
--- AndroidEatIt/app/src/main/java/io/github/joseseie/androideatit/viewHolder/MenuViewHolder.java
+++ AndroidEatIt/app/src/main/java/io/github/joseseie/androideatit/viewHolder/MenuViewHolder.java
@@ -5,6 +5,9 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import io.github.joseseie.androideatit.Interface.ItemClickListener;
+import io.github.joseseie.androideatit.R;
+
 /**
  * Created by Jose Seie on 1/12/2018.
  */
@@ -14,16 +17,23 @@ public class MenuViewHolder extends RecyclerView.ViewHolder implements View.OnCl
     public TextView txtMenuName;
     public ImageView imageView;
 
+    private ItemClickListener itemClickListener;
+
     public MenuViewHolder(View itemView) {
         super(itemView);
 
-        txtMenuName = (TextView) itemView.findViewById(R.id)
+        txtMenuName = (TextView) itemView.findViewById(R.id.menu_name);
+        imageView   = (ImageView) itemView.findViewById(R.id.imageView);
 
+        itemView.setOnClickListener(this);
     }
 
+    public void setItemClickListener(ItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
+    }
 
     @Override
     public void onClick(View view) {
-
+        itemClickListener.onClick(view, getAdapterPosition(), false);
     }
 }
