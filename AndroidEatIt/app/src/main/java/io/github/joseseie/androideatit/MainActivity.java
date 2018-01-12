41517f56e5bc1369e18ad0a9c1f49d3960fce5fb
Jose Seie <joseeugenioseie@hotmail.com>
2018-01-12 09:44:05 +0200

    Adicao das fontes e conclusao da tela, tendo bugs com emulador

--- AndroidEatIt/app/src/main/java/io/github/joseseie/androideatit/MainActivity.java
+++ AndroidEatIt/app/src/main/java/io/github/joseseie/androideatit/MainActivity.java
@@ -1,7 +1,9 @@
 package io.github.joseseie.androideatit;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -19,6 +21,22 @@ public class MainActivity extends AppCompatActivity {
         btnSignUp   = (Button) findViewById(R.id.btnSignUp);
 
         txtSlogan   = (TextView) findViewById(R.id.txtSlogan);
+
+        Typeface face =  Typeface.createFromAsset(getAssets(), "fonts/NABILA.TTF");
+        txtSlogan.setTypeface( face );
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
 
     }
