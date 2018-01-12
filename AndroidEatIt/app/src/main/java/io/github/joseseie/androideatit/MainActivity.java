80f21ab2291da6214055866b4b8afbc84a0a59ea
Jose Seie <joseeugenioseie@hotmail.com>
2018-01-12 08:24:00 +0200

    Recuperando os componentes do XML

--- AndroidEatIt/app/src/main/java/io/github/joseseie/androideatit/MainActivity.java
+++ AndroidEatIt/app/src/main/java/io/github/joseseie/androideatit/MainActivity.java
@@ -2,12 +2,24 @@ package io.github.joseseie.androideatit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn, btnSignUp;
+    TextView txtSlogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignIn   = (Button) findViewById(R.id.btnSignIn);
+        btnSignUp   = (Button) findViewById(R.id.btnSignUp);
+
+        txtSlogan   = (TextView) findViewById(R.id.txtSlogan);
+
+
     }
 }
