bf6d4487d4492b16ec4a47a05a6ff93d593866e2
Jose Seie <joseeugenioseie@hotmail.com>
2018-01-13 11:23:07 +0200

    Conclusao, fazendo registo, actualizacao e remocao de registos no firebase

--- MyFirebase/app/src/main/java/io/github/joseseie/myfirebase/MainActivity.java
+++ MyFirebase/app/src/main/java/io/github/joseseie/myfirebase/MainActivity.java
@@ -4,13 +4,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import io.github.joseseie.myfirebase.modelo.Pessoa;
@@ -23,6 +31,10 @@ public class MainActivity extends AppCompatActivity {
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
 
+    private List<Pessoa> listaPessoa = new ArrayList<>();
+    private ArrayAdapter<Pessoa> arrayAdapterPessoa;
+
+    Pessoa pessoaSelecionada;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,12 +46,51 @@ public class MainActivity extends AppCompatActivity {
         listV_dados = (ListView) findViewById(R.id.listV_dados);
 
         inicializaFirebase();
+
+        eventoDatabase();
+
+        listV_dados.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                pessoaSelecionada = (Pessoa) adapterView.getItemAtPosition(i);
+                edtNome.setText( pessoaSelecionada.getNome() );
+                edtEmail.setText( pessoaSelecionada.getEmail() );
+
+            }
+        });
+    }
+
+    private void eventoDatabase() {
+        databaseReference.child("Pessoa").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                listaPessoa.clear();
+                for (DataSnapshot objSnapshot: dataSnapshot.getChildren())
+                {
+                    Pessoa p = objSnapshot.getValue(Pessoa.class);
+                    listaPessoa.add( p );
+                }
+                arrayAdapterPessoa = new ArrayAdapter<Pessoa>(
+                        MainActivity.this,
+                        android.R.layout.simple_list_item_1,
+                        listaPessoa);
+                listV_dados.setAdapter( arrayAdapterPessoa );
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     private void inicializaFirebase() {
 
         FirebaseApp.initializeApp(MainActivity.this);
         firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseDatabase.setPersistenceEnabled( true );
         databaseReference = firebaseDatabase.getReference();
     }
 
@@ -63,6 +114,22 @@ public class MainActivity extends AppCompatActivity {
             databaseReference.child("Pessoa").child( p.getUid() ).setValue( p );
             limparCampos();
 
+        }
+        else if(id == R.id.menu_actualizar && pessoaSelecionada.getUid() != null)
+        {
+            Pessoa p = new Pessoa();
+            p.setUid( pessoaSelecionada.getUid() );
+            p.setNome( edtNome.getText().toString());
+            p.setEmail( edtEmail.getText().toString() );
+            databaseReference.child("Pessoa").child( p.getUid() ).setValue( p );
+            limparCampos();
+        }
+        else if(id == R.id.menu_deletar)
+        {
+            Pessoa p = new Pessoa();
+            p.setUid( pessoaSelecionada.getUid() );
+            databaseReference.child("Pessoa").child(p.getUid()).removeValue();
+            limparCampos();
         }
         return true;
     }
