77f15043e85c46c8d76e306118c8bb181bd805ba
Jose Seie <joseeugenioseie@hotmail.com>
2018-01-17 21:24:24 +0200

    Implementando a listagem com firebase-ui,
    Nao funcionada ainda, pois ainda estou a ter bugs

---
+++ FireApp/app/src/main/java/io/github/joseseie/fireapp/ListViewActivity.java
@@ -0,0 +1,37 @@
+package io.github.joseseie.fireapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class ListViewActivity extends AppCompatActivity {
+
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+
+        listView    = (ListView) findViewById(R.id.listView);
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://fireapp-59e43.firebaseio.com/");
+
+        FirebaseListAdapter<String> firebaseListAdapter = new FirebaseListAdapter<String>(
+                this,
+                String.class,
+                databaseReference
+        ) {
+            @Override
+            protected void populateView(View v, String model, int position) {
+
+            }
+        };
+
+    }
+}
