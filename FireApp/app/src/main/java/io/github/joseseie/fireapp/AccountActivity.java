4712dcc8cb3291e59c2bdde70f47d2dc279653e8
Jose Seie <joseeugenioseie@hotmail.com>
2018-01-18 12:05:14 +0200

    Logout implementado...

--- FireApp/app/src/main/java/io/github/joseseie/fireapp/AccountActivity.java
+++ FireApp/app/src/main/java/io/github/joseseie/fireapp/AccountActivity.java
@@ -1,13 +1,57 @@
 package io.github.joseseie.fireapp;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class AccountActivity extends AppCompatActivity {
+
+    private Button mLogOutBtn;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+        mLogOutBtn = (Button) findViewById(R.id.btnLogOut);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener =  new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                if (firebaseAuth.getCurrentUser() == null)
+                {
+                    startActivity(new Intent(AccountActivity.this, GoogleSignInActivity.class));
+                }
+
+            }
+        };
+
+        mLogOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mAuth.signOut();
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mAuth.addAuthStateListener( mAuthListener );
+
     }
 }
