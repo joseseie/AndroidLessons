05147048b1aa4d72a61475dad8193bec9e1cea53
Jose Seie <joseeugenioseie@hotmail.com>
2018-02-01 06:51:44 +0200

    Saving file on FirebaseStorage  e saving data on FirebaseDatabase

--- ProjectSimpleBlogApp/app/src/main/java/io/github/joseseie/projectsimpleblogapp/PostActivity.java
+++ ProjectSimpleBlogApp/app/src/main/java/io/github/joseseie/projectsimpleblogapp/PostActivity.java
@@ -1,24 +1,55 @@
 package io.github.joseseie.projectsimpleblogapp;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class PostActivity extends AppCompatActivity {
 
     private ImageButton mSelectImage;
+    private EditText mPostTitle, mPostDesc;
+
+    private Button mSubmitBtn;
+
+    private Uri imageUri = null;
 
     private static final int GALLERY_REQUEST = 1;
+
+    private StorageReference mStorage;
+    private DatabaseReference mDatabase;
+
+    private ProgressDialog mProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
 
+        mStorage = FirebaseStorage.getInstance().getReference();
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Blog");
+
         mSelectImage = (ImageButton) findViewById(R.id.imageSelect);
+
+        mPostDesc   = (EditText) findViewById(R.id.desField);
+        mPostTitle  = (EditText) findViewById(R.id.titleField);
+
+        mSubmitBtn  = (Button) findViewById(R.id.submitBtn);
+
+        mProgress = new ProgressDialog(this);
 
         mSelectImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,6 +61,45 @@ public class PostActivity extends AppCompatActivity {
             }
         });
 
+        mSubmitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startPosting();
+            }
+        });
+
+    }
+
+    private void startPosting() {
+        mProgress.setMessage("Posting to blog . . . ");
+        mProgress.show();
+        final String title_val = mPostTitle.getText().toString().trim();
+        final String desc_val = mPostDesc.getText().toString().trim();
+
+        if(!TextUtils.isEmpty(title_val) && !TextUtils.isEmpty(desc_val)  && imageUri != null)
+        {
+            StorageReference filepath = mStorage.child("Blog_Images").child(imageUri.getLastPathSegment());
+            filepath.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    Uri download = taskSnapshot.getDownloadUrl();
+
+                    DatabaseReference newPost = mDatabase.push();
+                    newPost.child("title").setValue(title_val);
+                    newPost.child("desc").setValue(desc_val);
+                    newPost.child("image").setValue(download.toString());
+
+                    mProgress.dismiss();
+
+                    startActivity( new Intent(PostActivity.this, MainActivity.class));
+                }
+            });
+
+
+        }
+
+
     }
 
     @Override
@@ -38,7 +108,7 @@ public class PostActivity extends AppCompatActivity {
 
         if (requestCode == GALLERY_REQUEST && resultCode == RESULT_OK)
         {
-            Uri imageUri = data.getData();
+            imageUri = data.getData();
             mSelectImage.setImageURI( imageUri );
         }
 
